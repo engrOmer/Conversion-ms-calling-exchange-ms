12daf137b83928e81006be4c0662ef15b2b81ad4
Omer Asif <enngromer1995@gmail.com>
2022-05-15 16:41:42 +0500

    Eurika load balancer for different instnaces of conversion exchnage service 8001 8090

--- currency-conversion-microservice/src/main/java/com/example/microservices/currencyconversionmicroservice/CurrencyExchangeProxy.java
+++ currency-conversion-microservice/src/main/java/com/example/microservices/currencyconversionmicroservice/CurrencyExchangeProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currncy-exchange",url ="localhost:8090")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion currencyExchange(@PathVariable String from, @PathVariable String to );
