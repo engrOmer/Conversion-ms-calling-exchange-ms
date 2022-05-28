7ae2bdbb836c69528803da9c4c48541ff046bd26
Omer Asif <enngromer1995@gmail.com>
2022-05-28 19:09:50 +0500

    Api configuration changes

---
+++ api-gateway/src/main/java/com/example/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        Function<PredicateSpec, Buildable<Route>> routeFunction = a -> a.path("/get")
+                .filters(f->f.addRequestHeader("MyHeader","MyURI")
+                .addRequestHeader("Param","MyValue"))
+                .uri("http://httpbin.org:88");
+        Function<PredicateSpec, Buildable<Route>> routeFunction2 = a -> a.path("/CURRENCY-EXCHANGE**")
+                .filters(f->f.addRequestHeader("MyHeader","MyURI")
+                        .addRequestHeader("Param","MyValue"))
+                .uri("lb:CURRENCY-EXCHANGE");
+
+
+        return builder.routes().route(routeFunction).build();
+
+    }
+}
